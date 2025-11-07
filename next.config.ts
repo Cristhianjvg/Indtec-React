3c0cc37c9df574cddb09750dc985bcbaec772504
Cristhianjvg <cristhianvillamarin2002@gmail.com>
2025-11-06 22:17:39 -0500

    arreglar problemas para hacer el build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // genera versión estática (SPA)
   images: { unoptimized: true }, // evita uso de Image Optimization (solo SSR)
+  trailingSlash: true,
   // trailingSlash: true, // asegura que las rutas funcionen en hosting compartido
   // reactStrictMode: true,
 };
