924307e13ffd16c9855fc2b2a60fbe6e1552e3a4
Cristhianjvg <cristhianvillamarin2002@gmail.com>
2025-11-18 17:40:36 -0500

    dsa

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
   output: "export", // genera versión estática (SPA)
   images: {
     unoptimized: true,
-    localPatterns: [
-      {
-        pathname: "/img/logos/:path*",
-        search: "v=*",
-      },
-    ],
   },
   trailingSlash: true,
   // trailingSlash: true, // asegura que las rutas funcionen en hosting compartido
